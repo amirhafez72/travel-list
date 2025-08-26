1410f5e53296d9d5060a481e937e9a2e9db1da08
Hossein Shamohammadi <amirhafez72@yahoo.com>
2025-08-26 20:53:28 +0330

    progress bar

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default function App() {
       <Logo /> 
       <Form onAddItems={hanldeAddItems}/>
       <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
-      <Stats />
+      <Stats items={items}/>
   </div>;
 }
 
@@ -78,9 +78,17 @@ function PackingList({items,onDeleteItem,onToggleItem}) {
   </div>;
 }
 
-function Stats() {
+function Stats({items}) {
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems)*100);
+  if(!items.length) return <p className='stats'><em>
+      Start adding  some items to your packing list🚀
+    </em></p>
+
+
   return <footer className='stats'>
-    <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    <em>{percentage === 100 ? 'You got everything! Ready to go ✈️' : `💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage} %)`}</em>
   </footer>;
 }
 
