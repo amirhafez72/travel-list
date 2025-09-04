9ace302c1660ea9a6da1ab4314f4dc91c5389e69
Hossein Shamohammadi <amirhafez72@yahoo.com>
2025-09-04 10:13:49 +0330

    sorting

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import "./index.css";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
 export default function App() {
-  const [items,setItems] = useState([]);
+  const [items,setItems] = useState(initialItems);
   function hanldeAddItems (item){
     setItems((items) => [...items,item]);
   }
@@ -71,10 +71,23 @@ function Form({onAddItems}) {
 
 
 function PackingList({items,onDeleteItem,onToggleItem}) {
+  const [sortBy,setSortBy] = useState("input");
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description") sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed") sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return <div className='list'>
     <ul>
-      {items.map((item) => (<Item item={item} key={item.id} onDeleteItem ={onDeleteItem} onToggleItem={onToggleItem} />))}
+      {sortedItems.map((item) => (<Item item={item} key={item.id} onDeleteItem ={onDeleteItem} onToggleItem={onToggleItem} />))}
     </ul>
+    <div className='actions'>
+      <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+        <option value="input">Sort by input order</option>  
+        <option value="description">Sort by description</option>  
+        <option value="packed">Sort by packed status</option>  
+      </select>      
+      </div>
   </div>;
 }
 
